daeab6cefe353e403f3d272d7dc93d595b96bd4d
Gurinder Batth <gurinderbatth@192.168.1.10>
2021-09-16 09:35:53 +0530

    Solved

--- dijsktra/code_working.cpp
+++ dijsktra/code_working.cpp
@@ -54,31 +54,8 @@ void initCurrentRow(int source){
     }
 }
 
-int getMin(int source){
-    int small = INT_MAX;
-    for(int i = 0; i <= 5 ; i++){
-         if( current_row[i] != -1 && current_row[i] < small){
-             small = current_row[i] ;
-         }
-    }
-    return small;
-}
-
-int getMinIndex(int source){
-    int small = INT_MAX;
-    int pos = -1;
-       for(int i = 0; i <= 5 ; i++){
-         if( current_row[i] != -1 && current_row[i] < small){
-             small = current_row[i] ;
-             pos = i;
-         }
-    }
-    return small;
-    return pos;
-}
 
 int * find(int source){
-    int arr[2];
     int small = INT_MAX;
     int pos = -1;
        for(int i = 0; i <= 5 ; i++){
@@ -87,68 +64,84 @@ int * find(int source){
              pos = i;
          }
     }
-    arr[0] = small;
-    arr[1] = pos;
+    int arr[2] = { small , pos };
     return arr;
 }
 
-int getLastVisited(){
-     return visited.at(visited.size()-1);
-}
-
 bool isAllVisited(){
-     for(int i = 0; i < unvisited.size(); i++){
-          if(unvisited.at(i) != -1){
-              return false;
-          }
+     for(int i = 0 ; i < unvisited.size() ; i++){
+         if(  unvisited.at(i) != -1 ){
+             return false;
+         }
      }
      return true;
+}
+
+int getLastElement(){
+    //  2
+    //  1 -> 4
+    //  2 - 1
+    // return 1
+    return visited.at( visited.size() - 1 );
+}
+
+bool isVisited(int val){
+    for(int i = 0 ; i < visited.size() ; i++){
+        if(visited.at(i) == val){
+            return true;
+        }
+    }
+    return false;
 }
 
 void dijkstra(int source){
      initArray(source);
      initCurrentRow(source);
      int *p = find(source);
-     int min_number = *p;
+     int min_number = *(p);
      int index = *(p+1);
      cout<<source<<"<-->"<<index<<" " <<min_number<<"KM "<<endl;
-     current_row[index] = -1;
      visited.push_back(index);
      unvisited[index] = -1;
-
-     int i = 0;
-     while(isAllVisited() == false && i < 10){
-          int last_el = getLastVisited();
-          for(int i = 0; i < unvisited.size(); i++){
-            if(unvisited.at(i) != -1 ){
-                int node = unvisited.at(i);
-                if( matrix[last_el][node] != 0 ){
-                    int ndis = min_number + matrix[last_el][node];
-                    int odis = current_row[node];
-                    /**
-                     * TODO: -1 means node distance is inifinty
-                     * */
-                    current_row[node] = odis == -1  ? ndis : ( ndis < odis ? ndis : odis );
-                }
-            }
-         }
-         int *p = find(source);
-         min_number = *p;
-         index = *(p+1);
-         cout<<source<<"<-->"<<index<<" " <<min_number<<"KM "<<endl;
-         current_row[index] = -1;
-         visited.push_back(index);
-         unvisited[index] = -1;
-         i++;
+     while(  isAllVisited() == false )
+     {
+          int last_ele = getLastElement();
+          for(int i = 0 ; i <= 5 ;  i++ ){
+              // here we check where to go
+              // eg : 4 -> 3
+              //  i = 3
+              if( matrix[last_ele][i] != 0 && isVisited(i) == false){
+                //   
+                  int odis = current_row[i];
+                  int ndis =  current_row[index] + matrix[last_ele][i];
+                  if(odis == -1){
+                        current_row[i] = current_row[index] + matrix[last_ele][i];
+                  }
+                  else
+                  {
+                      
+                       if( ndis < current_row[i]){
+                           current_row[i] = ndis ;
+                       }
+                  }
+              }
+          }
+          current_row[index] = -1;
+          int *p2 = find(source);
+          min_number = *(p2);
+          index = *(p2+1);
+          cout<<source<<"<-->"<<index<<" " <<min_number<<"KM "<<endl;
+          visited.push_back(index);
+          unvisited[index] = -1;
      }
 
 }
 
 int main(){
     addVertex(0,4,4);
-    addVertex(0,2,5);
+    addVertex(0,2,8);
     addVertex(1,3,2);
-    addVertex(2,5,2);
+    addVertex(2,5,6);
     addVertex(3,5,3);
     addVertex(3,4,5);
     printMatrix();
